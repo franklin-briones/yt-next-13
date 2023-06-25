7b63683693b139a5aa822dc55e5bc58443f58186
Franklin Briones <franklindbriones@gmail.com>
2023-06-25 14:56:05 -0500

    begin todo generatestaticparams

---
+++ app/todos/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Todoslist from './todoslist';
+
+function todos() {
+    return (
+        <div>
+            <Todoslist />
+        </div>
+    );
+}
+
+export default todos;
---
+++ app/todos/todoslist.tsx
@@ -0,0 +1,27 @@
+import { Todo } from '@/typings';
+import React from 'react';
+import Link from 'next/link';
+
+const fetchTodos = async() => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/")
+    const todos: Todo[] = await res.json()
+    return todos;
+}
+
+
+async function Todoslist() {
+    const todos = await fetchTodos()
+    return (
+        <>
+        {todos.map((todo)=> (
+            <p key = {todo.id}>
+                <Link href={`/todos/${todo.id}`}>Todo: {todo.id}</Link>
+            </p>
+        ))}
+        </>
+    );
+}
+
+export default Todoslist;
+
+
